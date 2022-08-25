f1196f106d152dcdc3b89dbebcb681d08209fecb
Armin Ariamajd <armin.ariam@fu-berlin.de>
2022-08-25 09:10:49 +0200

    rename parameters in classes `FIFOQueue` and `LIFOQueue` to conform with the base class

--- alds/data_structures/queue.py
+++ alds/data_structures/queue.py
@@ -7,11 +7,11 @@ from .abc import Queue
 class FIFOQueue(Queue):
     """A queue data structure with a first-in-first-out (FIFO) policy."""
 
-    def __init__(self, init_queue: Optional[list] = None):
-        if init_queue is None:
+    def __init__(self, initial_queue: Optional[list[Any]] = None):
+        if initial_queue is None:
             self._data = []
-        elif isinstance(init_queue, list):
-            self._data = init_queue
+        elif isinstance(initial_queue, list):
+            self._data = initial_queue.copy()
         else:
             raise TypeError("Argument `init_queue` must be of type `list`.")
         return
@@ -29,9 +29,9 @@ class FIFOQueue(Queue):
     def pop(self):
         return self._data.pop()
 
-    def has_element(self, elem: Any, key: Callable = lambda elem: elem) -> bool:
-        for e in self._data:
-            if key(e) == elem:
+    def has_element(self, element: Any, key: Callable = lambda elem: elem) -> bool:
+        for elem in self._data:
+            if key(elem) == element:
                 return True
         return False
 
@@ -44,20 +44,23 @@ class FIFOQueue(Queue):
         return len(self._data) == 0
 
 
-class LIFOQueue:
+class LIFOQueue(Queue):
     """A queue data structure with a last-in-first-out (LIFO) policy, also known as a stack."""
 
-    def __init__(self, init_queue: Optional[list] = None):
-        if init_queue is None:
+    def __init__(self, initial_queue: Optional[list[Any]] = None):
+        if initial_queue is None:
             self._data = []
-        elif isinstance(init_queue, list):
-            self._data = init_queue
+        elif isinstance(initial_queue, list):
+            self._data = initial_queue.copy()
         else:
             raise TypeError("Argument `init_queue` must be of type `list`.")
         return
 
     def __str__(self):
         return ", ".join([str(elem) for elem in self._data[-1::-1]])
+
+    def __repr__(self):
+        return f"{type(self).__name__}({self._data})"
 
     def push(self, element: Any) -> NoReturn:
         self._data.append(element)
@@ -66,9 +69,9 @@ class LIFOQueue:
     def pop(self) -> Any:
         return self._data.pop()
 
-    def has_element(self, elem: Any, key: Callable = lambda elem: elem) -> bool:
-        for e in self._data:
-            if key(e) == elem:
+    def has_element(self, element: Any, key: Callable = lambda elem: elem) -> bool:
+        for elem in self._data:
+            if key(elem) == element:
                 return True
         return False
 
